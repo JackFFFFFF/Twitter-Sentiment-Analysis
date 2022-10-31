3db645a8a224177efd918737ab4c347d276a48f1
JackFFFFFF <n10794948@qut.edu.au>
2022-10-31 20:38:55 +1000

    Rules are now retrieved from S3 without calling API

--- TwitterTradingSentiment/storage_handler.js
+++ TwitterTradingSentiment/storage_handler.js
@@ -21,42 +21,78 @@ s3.createBucket({ Bucket: bucketName })
       console.log(`Error creating bucket: ${err}`);
     }
   });
+async function storeObject(data) {
+  //Create object upload promise
+  const key = data.symbol;
+  const s3Key = `stock-${key}`;
+  const params = {
+    Bucket: bucketName,
+    Key: s3Key,
+    ContentType: "application/json",
+    Body: JSON.stringify({ source: "S3 Bucket", ...data }),
+  };
+  await s3
+    .putObject(params)
+    .promise()
+    .then(() => {
+      console.log(`Successfully uploaded data to ${bucketName}/${s3Key}`);
+    })
+    .catch((err) => {
+      console.log(err, err.stack);
+    });
+}
+async function retrieveObject(symbol) {
+  const key = symbol;
+  const s3Key = `stock-${key}`;
+  const params = { Bucket: bucketName, Key: s3Key };
+  return await s3
+    .getObject(params)
+    .promise()
+    .then((result) => {
+      const resultJSON = JSON.parse(result.Body.toString("utf-8"));
+      return resultJSON;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+function retreiveKeys() {
+  //copied from here https://stackoverflow.com/a/69754448/8096569
+  return new Promise((resolve, reject) => {
+    try {
+      let params = {
+        Bucket: bucketName,
+        MaxKeys: 1000,
+      };
+      const allKeys = [];
+      listAllKeys();
+      function listAllKeys() {
+        s3.listObjectsV2(params, function (err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            var contents = data.Contents;
+            contents.forEach(function (content) {
+              allKeys.push(content.Key);
+            });
 
+            if (data.IsTruncated) {
+              params.ContinuationToken = data.NextContinuationToken;
+              console.log("get further list...");
+              listAllKeys();
+            } else {
+              resolve(allKeys);
+            }
+          }
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
 module.exports = {
-  storeObject: function (data) {
-    //Create object upload promise
-    const key = data.symbol;
-    const s3Key = `stock-${key}`;
-    const params = {
-      Bucket: bucketName,
-      Key: s3Key,
-      ContentType: "application/json",
-      Body: JSON.stringify({ source: "S3 Bucket", ...data }),
-    };
-    s3.putObject(params)
-      .promise()
-      .then(() => {
-        console.log(`Successfully uploaded data to ${bucketName}/${s3Key}`);
-      })
-      .catch((err) => {
-        console.log(err, err.stack);
-      });
-  },
-  retrieveObject: async function (symbol) {
-    const key = symbol;
-    const s3Key = `stock-${key}`;
-    const params = { Bucket: bucketName, Key: s3Key };
-    return await s3
-      .getObject(params)
-      .promise()
-      .then((result) => {
-        // Serve from S3
-        //console.log(result);
-        const resultJSON = JSON.parse(result.Body.toString("utf-8"));
-        return resultJSON;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
+  storeObject,
+  retrieveObject,
+  retreiveKeys,
 };
