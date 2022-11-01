82e9c97a5167b03457c500daab91fee3f1d943d8
JackFFFFFF <n10794948@qut.edu.au>
2022-11-01 17:51:30 +1000

    Adding communication module

--- TwitterSentimentAnalysis/routes/index.js
+++ TwitterSentimentAnalysis/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  rawTweet = req;
+  res.render("index", { title: "Express", tweet: rawTweet });
 });
 
 module.exports = router;
