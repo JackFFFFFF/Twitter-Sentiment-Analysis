eebacf02d1174cb6d6ef384c744a683491a43935
Jayden Lyttle <85596245+j-lyttle@users.noreply.github.com>
2022-11-03 22:22:58 +1000

    Adds searching and passing initial page
    Also fixed crashing error

--- TwitterTradingSentiment/routes/specific.js
+++ TwitterTradingSentiment/routes/specific.js
@@ -1,9 +1,30 @@
-var express = require('express');
+const e = require("express");
+var express = require("express");
 var router = express.Router();
+var storageHandler = require("../storage_handler");
+var stockHandler = require("../stock_handler");
+var twitterHandler = require("../twitter_handler");
 
-/* GET users listing. */
-router.get('/:stockCode', function(req, res, next) {
-  res.send(req.params.stockCode);
+/* GET home page. */
+router.get("/:stockIndex", async function (req, res, next) {
+  await stockHandler.getTickers().then((result)=>{})
+  await storageHandler.retreiveKeys().then(async (keys) => {
+    await stockHandler.makeRules(keys).then(async (rules) => {
+      const stockCode = rules[req.params.stockIndex].tag;
+      const stockCode2 = stockCode.split("/")[0];
+      await storageHandler.retrieveObject(stockCode2).then((stockVals) => {
+        console.log(rules);
+
+        res.render("specific", { ruleList: rules, index: req.params.stockIndex, data: stockVals });
+        twitterHandler.startStream(rules);
+      });
+      
+    });
+  });
+
+  
 });
-
+function PrintJSON(json) {
+  console.log(json);
+}
 module.exports = router;
