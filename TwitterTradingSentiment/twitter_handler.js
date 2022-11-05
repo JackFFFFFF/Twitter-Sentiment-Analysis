9007569c2d517966677400e84619712f1ccc09ce
JackFFFFFF <n10794948@qut.edu.au>
2022-11-05 11:28:31 +1000

    Doing some stuff yo

--- TwitterTradingSentiment/twitter_handler.js
+++ TwitterTradingSentiment/twitter_handler.js
@@ -85,28 +85,32 @@ function streamConnect(retryAttempt) {
   //TODO: Erorr Handle this so it doesn't crash
   stream
     .on("data", (data) => {
-      try{
+      try {
         //console.log(data);
-        if(data.title == 'ConnectionException'){
+        if (data.title == "ConnectionException") {
           console.log("Connection errored out!");
           console.log(e);
           process.exit(1);
-        }else{
+        } else if (data.title == "Too Many Requests" || data.status == 429) {
+          setTimeout(() => {
+            console.warn("Too many tweets coming through");
+            streamConnect(++retryAttempt);
+          }, 10 ** retryAttempt);
+        } else {
           const json = JSON.parse(data);
           commsHandler.sendData(json); //Send post of tweet text to server
           console.log(json);
           // A successful connection resets retry count.
           retryAttempt = 0;
         }
-      }catch{
+      } catch (err) {
+        //console.log(err);
         console.log("Stream overloaded " + retryAttempt);
         setTimeout(() => {
           console.warn("A connection error occurred. Reconnecting...");
           streamConnect(++retryAttempt);
         }, 2 ** retryAttempt);
       }
-      
-
     })
     .on("err", (error) => {
       if (error.code !== "ECONNRESET") {
